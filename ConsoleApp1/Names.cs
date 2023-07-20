9a2e820f4d6f5eed6a4b1a6b3c5554d5c359c2cb
Peti <Peti@DESKTOP-N3G2RBD>
2023-07-20 23:43:24 +0200

    Add project files.

---
+++ ConsoleApp1/Names.cs
@@ -0,0 +1,140 @@
+﻿using Ekko;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Names
+    {
+        List<PlayerChampionSelection> names;
+        private LeagueApi api;
+        public Names(LeagueApi api)
+        {
+            this.api = api;
+        }
+        public async Task<(List<PlayerChampionSelection>, long,string)> GetNames()
+        {
+            names = new List<PlayerChampionSelection>();
+            var z = await api.SendAsync(HttpMethod.Get, "/lol-summoner/v1/current-summoner");
+            var d = await api.SendAsync(HttpMethod.Get, "/lol-gameflow/v1/session");
+
+            PlayerAccount account = JsonConvert.DeserializeObject<PlayerAccount>(z);
+            Root root = JsonConvert.DeserializeObject<Root>(d);
+
+            if (account == null || root == null)
+            {
+                return (null, 0,null);
+            }
+            GameData gameData = root.gameData;
+            string displayName = account.displayName;
+            long summonerId = account.summonerId;
+            if (gameData == null || gameData.playerChampionSelections.Length != 10)
+            {
+                return (null, 0,null);
+            }
+
+            int index = 10;
+            for (int i = 0; i < gameData.playerChampionSelections.Length; i++)
+            {
+                if (gameData.playerChampionSelections[i].summonerInternalName == account.displayName)
+                {
+                    index = i;
+                }
+            }
+            if (index == 10)
+            {
+                return (null, 0,null);
+            }
+            int team;
+            if (index >= 5)
+            {
+                team = 1;
+            }
+            else
+            {
+                team = 0;
+            }// if player index is >5 then team 2, if less then team 1
+            for (int i = (team == 1 ? 5 : 0); i < (team == 1 ? 10 : 5); i++)
+            {
+                //Console.WriteLine(gameData.playerChampionSelections[i]);
+                names.Add(gameData.playerChampionSelections[i]);
+            }
+            //Console.WriteLine(gameData.playerChampionSelections[0]);
+
+            // Access the extracted fields
+
+
+            // Output the values
+            //Console.WriteLine("Display Name: " + displayName);
+            //Console.WriteLine("Summoner ID: " + summonerId);
+            return (names, gameData.gameId,team ==0? "Blue" : "Red");
+        }
+    }
+    public class InGamePlayer
+    {
+        public string summonerInternalName { get; set; }
+        public int championId { get; set; }
+    }
+
+
+    public class PlayerAccount
+    {
+        public string displayName { get; set; }
+        public long summonerId { get; set; }
+    }
+
+    public class GameClient
+    {
+        public string observerServerIp { get; set; }
+        public int observerServerPort { get; set; }
+        public bool running { get; set; }
+        public string serverIp { get; set; }
+        public int serverPort { get; set; }
+        public bool visible { get; set; }
+    }
+
+    public class PlayerChampionSelection
+    {
+        public int championId { get; set; }
+        public int selectedSkinIndex { get; set; }
+        public int spell1Id { get; set; }
+        public int spell2Id { get; set; }
+        public string summonerInternalName { get; set; }
+    }
+
+    public class GameData
+    {
+        public long gameId { get; set; }
+        public string gameName { get; set; }
+        public bool isCustomGame { get; set; }
+        public string password { get; set; }
+        public PlayerChampionSelection[] playerChampionSelections { get; set; }
+    }
+
+    public class QueueAvailability
+    {
+        public bool areFreeChampionsAllowed { get; set; }
+        public string description { get; set; }
+        public string detailedDescription { get; set; }
+        public string gameMode { get; set; }
+        public bool isRanked { get; set; }
+        public bool isTeamBuilderManaged { get; set; }
+        public string name { get; set; }
+        public int numPlayersPerTeam { get; set; }
+        public string queueAvailability { get; set; }
+        public bool spectatorEnabled { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Root
+    {
+        public GameClient gameClient { get; set; }
+        public GameData gameData { get; set; }
+        public QueueAvailability queue { get; set; }
+        public string phase { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,314 @@
+﻿using System.Text;
+using ConsoleApp1;
+using Ekko;
+using LobbyReveal;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+public class Userinfo
+{
+    public string userinfo { get; set; }
+}
+public class currentPlatformId
+{
+    public string platform { get; set; }
+}
+public class Lol
+{
+    public string cpid { get; set; }
+}
+
+public class UserinfoActual
+{
+    public Lol lol { get; set; }
+
+}
+class gameData
+{
+    public string gamedata { get; set; }
+}
+
+
+
+public class alma1
+{
+    private string region;
+    private string[] lobbyNames;
+    public void Start()
+    {
+        new Thread(async () => { await Main1(); })
+        {
+            IsBackground = true
+        }.Start();
+    }
+    public async static Task Main(string[] args)
+    {
+        alma1 alma = new alma1();
+        alma.Start();
+        while (true)
+        {
+            var input = Console.ReadKey(true);
+        }
+    }
+    public async Task Main1()
+    {
+        Console.Title = "Lol VoiceChat";
+        Console.ForegroundColor = ConsoleColor.White;
+        var token = new CancellationTokenSource();
+        var watcher = new LeagueClientWatcher();
+        watcher.OnLeagueClient += async (clientWatcher, client) =>
+        {
+
+
+            Console.Clear();
+            token.Cancel();
+            var api1 = new LeagueApi(client.ClientAuthInfo.RiotClientAuthToken, client.ClientAuthInfo.RiotClientPort);
+            var api2 = new LeagueApi(client.ClientAuthInfo.RemotingAuthToken, client.ClientAuthInfo.RemotingPort);
+            //onsole.WriteLine("Making sure you are logged in...");
+            Regions regions = new Regions();
+            region = await regions.GetRegion(api1);
+            //Console.WriteLine(region);                             CANT USE THIS :( FORBIDDEN API REQUEST
+            /*
+            GetParticipants getp = new GetParticipants();
+            lobbyNames = await getp.GetNames(api1);
+
+            if (lobbyNames.Length == 0)
+            {
+                Console.WriteLine("Api not working, or not in lobby");
+            }
+            else
+            {
+                //Console.WriteLine(names);
+                string msg = $"[{region}]";
+                msg += "\n";
+                msg += string.Join(",", lobbyNames);
+                myWebhook webhook = new myWebhook();
+                webhook.sendWebHook("https://discord.com/api/webhooks/1130122431856648202/4A_N8ZgaFji5X5mzwUmKil2AcSPxVETsLkWU9SPXyuST1Fb445eyxCUtGlt7GTXuB1H7", msg, "Captain Hook");
+                Console.Clear();
+                Console.WriteLine("Voice Channel Created");
+                Console.WriteLine("You can now close this window");
+            }
+            */
+            Names inGameNames = new Names(api2);
+
+            var result = await inGameNames.GetNames();
+            if (result == (null, 0,null))
+            {
+                Console.WriteLine("Please run the program when you are loading in or in game");
+                Console.WriteLine("You can now close this window");
+            }
+            else
+            {
+                var players = result.Item1;
+                var gamid = result.Item2;
+                string msg = $"[{region}]";
+                msg += ";";
+                msg += result.Item3;
+                msg += ";";
+                msg += gamid;
+                msg += ";";
+                foreach (var player in players)
+                {
+                    msg += player.summonerInternalName;
+                    if (!(players.IndexOf(player) == players.Count-1))
+                    {
+                        msg += ",";
+                    }
+                }
+                msg += ";";
+                foreach (var player in players)
+                {
+                    
+                    ChampionIds champion = (ChampionIds)Enum.Parse(typeof(ChampionIds), player.championId.ToString());
+
+                    
+                    msg += champion.ToString();
+                    if (!(players.IndexOf(player) == players.Count-1))
+                    {
+                        msg += ",";
+                    }
+                }
+                myWebhook webhook = new myWebhook();
+                webhook.sendWebHook("https://discord.com/api/webhooks/1130122431856648202/4A_N8ZgaFji5X5mzwUmKil2AcSPxVETsLkWU9SPXyuST1Fb445eyxCUtGlt7GTXuB1H7", msg, "Captain Hook");
+                Console.Clear();
+                Console.WriteLine("Voice Channel Created");
+                Console.WriteLine("You can now close this window");
+            }
+            
+
+            Console.ReadLine();
+            Environment.Exit(0);
+        };
+        Console.WriteLine("Waiting for league client!");
+        await watcher.Observe(token.Token);
+        await Task.Delay(-1);
+    }
+    enum ChampionIds
+    {
+        Aatrox = 266,
+        Ahri = 103,
+        Akali = 84,
+        Akshan = 166,
+        Alistar = 12,
+        Amumu = 32,
+        Anivia = 34,
+        Annie = 1,
+        Aphelios = 523,
+        Ashe = 22,
+        AurelionSol = 136,
+        Azir = 268,
+        Bard = 432,
+        Belveth = 200,
+        Blitzcrank = 53,
+        Brand = 63,
+        Braum = 201,
+        Caitlyn = 51,
+        Camille = 164,
+        Cassiopeia = 69,
+        Chogath = 31,
+        Corki = 42,
+        Darius = 122,
+        Diana = 131,
+        Draven = 119,
+        DrMundo = 36,
+        Ekko = 245,
+        Elise = 60,
+        Evelynn = 28,
+        Ezreal = 81,
+        Fiddlesticks = 9,
+        Fiora = 114,
+        Fizz = 105,
+        Galio = 3,
+        Gangplank = 41,
+        Garen = 86,
+        Gnar = 150,
+        Gragas = 79,
+        Graves = 104,
+        Gwen = 887,
+        Hecarim = 120,
+        Heimerdinger = 74,
+        Illaoi = 420,
+        Irelia = 39,
+        Ivern = 427,
+        Janna = 40,
+        JarvanIV = 59,
+        Jax = 24,
+        Jayce = 126,
+        Jhin = 202,
+        Jinx = 222,
+        Kaisa = 145,
+        Kalista = 429,
+        Karma = 43,
+        Karthus = 30,
+        Kassadin = 38,
+        Katarina = 55,
+        Kayle = 10,
+        Kayn = 141,
+        Kennen = 85,
+        Khazix = 121,
+        Kindred = 203,
+        Kled = 240,
+        KogMaw = 96,
+        KSante = 897,
+        Leblanc = 7,
+        LeeSin = 64,
+        Leona = 89,
+        Lillia = 876,
+        Lissandra = 127,
+        Lucian = 236,
+        Lulu = 117,
+        Lux = 99,
+        Malphite = 54,
+        Malzahar = 90,
+        Maokai = 57,
+        MasterYi = 11,
+        Milio = 902,
+        MissFortune = 21,
+        MonkeyKing = 62,
+        Mordekaiser = 82,
+        Morgana = 25,
+        Nami = 267,
+        Nasus = 75,
+        Nautilus = 111,
+        Neeko = 518,
+        Nidalee = 76,
+        Nilah = 895,
+        Nocturne = 56,
+        Nunu = 20,
+        Olaf = 2,
+        Orianna = 61,
+        Ornn = 516,
+        Pantheon = 80,
+        Poppy = 78,
+        Pyke = 555,
+        Qiyana = 246,
+        Quinn = 133,
+        Rakan = 497,
+        Rammus = 33,
+        RekSai = 421,
+        Rell = 526,
+        Renata = 888,
+        Renekton = 58,
+        Rengar = 107,
+        Riven = 92,
+        Rumble = 68,
+        Ryze = 13,
+        Samira = 360,
+        Sejuani = 113,
+        Senna = 235,
+        Seraphine = 147,
+        Sett = 875,
+        Shaco = 35,
+        Shen = 98,
+        Shyvana = 102,
+        Singed = 27,
+        Sion = 14,
+        Sivir = 15,
+        Skarner = 72,
+        Sona = 37,
+        Soraka = 16,
+        Swain = 50,
+        Sylas = 517,
+        Syndra = 134,
+        TahmKench = 223,
+        Taliyah = 163,
+        Talon = 91,
+        Taric = 44,
+        Teemo = 17,
+        Thresh = 412,
+        Tristana = 18,
+        Trundle = 48,
+        Tryndamere = 23,
+        TwistedFate = 4,
+        Twitch = 29,
+        Udyr = 77,
+        Urgot = 6,
+        Varus = 110,
+        Vayne = 67,
+        Veigar = 45,
+        Velkoz = 161,
+        Vex = 711,
+        Vi = 254,
+        Viego = 234,
+        Viktor = 112,
+        Vladimir = 8,
+        Volibear = 106,
+        Warwick = 19,
+        Xayah = 498,
+        Xerath = 101,
+        XinZhao = 5,
+        Yasuo = 157,
+        Yone = 777,
+        Yorick = 83,
+        Yuumi = 350,
+        Zac = 154,
+        Zed = 238,
+        Zeri = 221,
+        Ziggs = 115,
+        Zilean = 26,
+        Zoe = 142,
+        Zyra = 143
+    }
+}
